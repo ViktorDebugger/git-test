9830e10757ed1f625cc5159377f3b9377bcc7f25
Viktor <viktor.luka.dev@gmail.com>
2025-06-10 08:56:20 +0300

    multiply commit

--- test.js
+++ test.js
@@ -8,6 +8,10 @@ function subtract(a, b) {
   return a - b;
 }
 
+function multiply(a, b) {
+    return a * b;
+}
+
 function divide(a, b) {
     if (b === 0) throw new Error("Division by zero");
     return a / b;
